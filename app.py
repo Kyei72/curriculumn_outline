0b3ff9804fa8c1377a38add3ee83f67a86d8f012
Kyei72 <bernardkyei72@gmail.com>
2025-12-23 14:00:56 +0000

    Day 3: Add student registration page + homepage redesign

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
-if __name__ == "__main__":
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+
+        name = request.form['name']
+        email = request.form['email']
+        class_name = request.form['class']
+        password = request.form['password']
+        print(name, email, class_name, password)  
+        return redirect(url_for('home'))
+    return render_template('register.html')
+
+if __name__ == '__main__':
     app.run(debug=True)
